a4de8453f3c61650c6371a19b098b60040aa4878
PelucheLab <kiwi.fruit.pochi@gmail.com>
2021-01-12 14:50:25 +0900

    新規作成

---
+++ Assets/Script/Egg.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Egg : MonoBehaviour
+{
+    [SerializeField] Sprite HiyokoEgg = null;
+    [SerializeField] Sprite Hiyoko = null;
+    
+    AudioSource audioSource;
+    public AudioClip sound1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -5.0f){
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public void Touch()
+    {
+        //this.GetComponent<Image>().sprite = HiyokoEgg;
+        StartCoroutine(Matteru());
+        //this.GetComponent<Image>().sprite = Hiyoko;
+		audioSource.PlayOneShot(sound1);
+
+        //Destroy (this.gameObject);
+
+    }
+
+	IEnumerator Matteru() 
+    {
+        this.GetComponent<Image>().sprite = HiyokoEgg;
+		yield return new WaitForSeconds(0.2f);
+        this.GetComponent<Image>().sprite = Hiyoko;
+    }
+
+}
---
+++ Assets/Script/EggGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggGenerator : MonoBehaviour
+{
+
+    [SerializeField] GameObject prefab = null;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartCoroutine(Generate());
+    }
+
+	IEnumerator Generate() {
+		yield return new WaitForSeconds(1.5f);
+        GameObject Egg = Instantiate(prefab, transform) as GameObject;
+        Egg.transform.position = new Vector3(-200.0f, 0.0f, 0);
+        Egg.transform.SetParent (canvas.transform, false); 
+        Rigidbody2D rb = Egg.GetComponent<Rigidbody2D> ();  // rigidbodyを取得
+        Vector2 force = new Vector2 (10.0f,0.0f);    // 力を設定
+        rb.AddForce(Vector2.left * -30.0f, ForceMode2D.Impulse);
+		StartCoroutine(Generate());
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    
+    }
+}
