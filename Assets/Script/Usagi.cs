fc0b175fe5172f63013bb19ab263f164652891c8
PelucheLab <kiwi.fruit.pochi@gmail.com>
2021-02-08 18:15:14 +0900

    うさぎに音追加

--- Assets/Script/Usagi.cs
+++ Assets/Script/Usagi.cs
@@ -4,34 +4,25 @@ using UnityEngine;
 
 public class Usagi : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Update()
-    {
-        
-        //if (Input.GetMouseButtonDown(0))
-        //{
-            
-          //  Rigidbody2D rig = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
-            //rig.AddForce(Vector2.right * 100);
+    AudioSource audioSource;
+    public AudioClip sound1;
 
-        //}
-        
-    }
-    void OnCollisionEnter(Collision collision)
+    void Start()
     {
-        Debug.Log("当たった!");
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void touch_right(){
-        Debug.Log("右をタッチ");
-        Rigidbody2D rig = transform.GetChild(2).gameObject.GetComponent<Rigidbody2D>();
+        audioSource.PlayOneShot(sound1);
+        Rigidbody2D rig = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
         rig.AddForce(Vector2.left * 200);
+
 
 
     }
     public void touch_left(){
-        Debug.Log("左をタッチ");
-        Rigidbody2D rig = transform.GetChild(2).gameObject.GetComponent<Rigidbody2D>();
+        audioSource.PlayOneShot(sound1);
+        Rigidbody2D rig = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
         rig.AddForce(Vector2.right * 200);
 
 
